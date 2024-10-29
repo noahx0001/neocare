1b47b9fa53fe21be765285f03d52786d2aa43eb5
noahx001 <lopeznoe191119@gmail.com>
2024-10-29 10:35:17 -0600

    Migraciones | 1.0.9

---
+++ database/migrations/1730208909896_create_chequeos_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'chequeos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('bebe_incubadora_id').notNullable().unsigned().references('id').inTable('bebes_incubadoras').onDelete('CASCADE')
+      table.integer('enfermera_id').notNullable().unsigned().references('id').inTable('enfermeras').onDelete('CASCADE')
+      table.string('descripcion')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
