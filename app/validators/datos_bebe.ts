ce64eb213800b32b00129e42006836be72e27128
JuanGmz <juanalfredohd@gmail.com>
2024-10-29 11:46:59 -0600

    DatosBebeValidator v1.0.0

---
+++ app/validators/datos_bebe.ts
@@ -0,0 +1,13 @@
+import vine from '@vinejs/vine'
+
+export const rolPost = vine.compile(
+    vine.object({
+        id: vine.number(),
+        bebe_incubadora_id: vine.number(),
+        oxigenacion: vine.number(),
+        pulsaciones: vine.number(),
+        temperatura_ambiental: vine.number(),
+        temperatura_corporal: vine.number(),
+        fecha_registro: vine.date(),
+    })
+)
